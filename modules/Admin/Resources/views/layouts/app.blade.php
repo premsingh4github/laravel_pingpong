7d2d049ee51901e24410cb318a377e9cfd13a78c
Prem Singh <prem.singh@peacenepal.com>
2015-11-29 08:49:08 +0545

    laravael with pingpong and auth module

--- modules/Admin/Resources/views/layouts/app.blade.php
+++ modules/Admin/Resources/views/layouts/app.blade.php
@@ -6,8 +6,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Module Admin</title>
 		{!!Html::style('../modules/Admin/Assets/bower_components/bootstrap/dist/css/bootstrap.min.css')!!}
-
-
 		{!!Html::style('../modules/Admin/Assets/bower_components/metisMenu/dist/metisMenu.min.css')!!}
 		{!!Html::style('../modules/Admin/Assets/dist/css/timeline.css')!!}
 		{!!Html::style('../modules/Admin/Assets/dist/css/sb-admin-2.css')!!}
@@ -38,21 +36,5 @@
     {!!Html::script('../modules/Admin/Assets/js/morris-data.js')!!}
     <!-- Custom Theme JavaScript -->
     {!!Html::script('../modules/Admin/Assets/dist/js/sb-admin-2.js')!!}
-     <!-- jQuery -->
-    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="../bower_components/raphael/raphael-min.js"></script>
-    <script src="../bower_components/morrisjs/morris.min.js"></script>
-    <script src="../js/morris-data.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
 	</body>
 </html>
